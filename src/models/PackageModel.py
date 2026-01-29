3eec3fc7376b4f0d99ff161199302847b731526f
iamugurduman <127699986+iamugurduman@users.noreply.github.com>
2026-01-29 09:10:56 +0300

    Update PackageModel.py

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,11 +1,11 @@
+
 from pydantic import Field, validator
 from typing import List, Optional, Union, Literal
 from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
 
-# Inputs-Outputs(With Validators)
 
-class InputImageOne(Input):
-    name: Literal["inputImageOne"] = "inputImageOne"
+class InputImage(Input):
+    name: Literal["inputImage"] = "inputImage"
     value: Union[List[Image], Image]
     type: str = "object"
 
@@ -18,12 +18,12 @@ class InputImageOne(Input):
             return "list"
 
     class Config:
-        title = "Image Input 1"
+        title = "Image"
 
 
-class InputImageTwo(Input):
-    name: Literal["inputImageTwo"] = "inputImageTwo"
-    value: Union[List[Image], Image]
+class OutputImage(Output):
+    name: Literal["outputImage"] = "outputImage"
+    value: Union[List[Image],Image]
     type: str = "object"
 
     @validator("type", pre=True, always=True)
@@ -35,270 +35,109 @@ class InputImageTwo(Input):
             return "list"
 
     class Config:
-        title = "Image Input 2"
+        title = "Image"
 
 
-class OutputImageOne(Output):
-    name: Literal["outputImageOne"] = "outputImageOne"
-    value: Union[List[Image], Image]
-    type: str = "object"
-
-    @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, value, values):
-        value = values.get('value')
-        if isinstance(value, Image):
-            return "object"
-        elif isinstance(value, list):
-            return "list"
+class KeepSideFalse(Config):
+    name: Literal["False"] = "False"
+    value: Literal[False] = False
+    type: Literal["bool"] = "bool"
+    field: Literal["option"] = "option"
 
     class Config:
-        title = "Output Image 1"
+        title = "Disable"
 
 
-class OutputImageTwo(Output):
-    name: Literal["outputImageTwo"] = "outputImageTwo"
-    value: Union[List[Image], Image]
-    type: str = "object"
-
-    @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, value, values):
-        value = values.get('value')
-        if isinstance(value, Image):
-            return "object"
-        elif isinstance(value, list):
-            return "list"
+class KeepSideTrue(Config):
+    name: Literal["True"] = "True"
+    value: Literal[True] = True
+    type: Literal["bool"] = "bool"
+    field: Literal["option"] = "option"
 
     class Config:
-        title = "Output Image 2"
+        title = "Enable"
 
 
-
-# Single Filter Configs
-# Blur
-class BlurKernelSize(Config):
-    name: Literal["BlurKernelSize"] = "BlurKernelSize"
-    value: int = Field(default=5)
-    type: Literal["integer"] = "integer"
-    field: Literal["textInput"] = "textInput"
+class KeepSideBBox(Config):
+    """
+        Rotate image without catting off sides.
+    """
+    name: Literal["KeepSide"] = "KeepSide"
+    value: Union[KeepSideTrue, KeepSideFalse]
+    type: Literal["object"] = "object"
+    field: Literal["dropdownlist"] = "dropdownlist"
 
     class Config:
-        title = "Kernel Size"
+        title = "Keep Sides"
 
-class BlurSigma(Config):
-    name: Literal["BlurSigma"] = "BlurSigma"
-    value: float = Field(default=0.0)
+
+class Degree(Config):
+    """
+        Positive angles specify counterclockwise rotation while negative angles indicate clockwise rotation.
+    """
+    name: Literal["Degree"] = "Degree"
+    value: int = Field(ge=-359.0, le=359.0,default=0)
     type: Literal["number"] = "number"
     field: Literal["textInput"] = "textInput"
+    placeHolder: Literal["[-359, 359]"] = "[-359, 359]"
 
     class Config:
-        title = "Sigma X"
-
-class OptionBlur(Config):
-    blurKernelSize: BlurKernelSize
-    blurSigma: BlurSigma
-    name: Literal["Blur"] = "Blur"
-    value: Literal["Blur"] = "Blur"
-    type: Literal["string"] = "string"
-    field: Literal["option"] = "option"
-
-    class Config:
-        title = "Gaussian Blur"
+        title = "Angle"
 
 
-#Edge
-class EdgeThreshold(Config):
-    name: Literal["EdgeThreshold"] = "EdgeThreshold"
-    value: int = Field(default=100)
-    type: Literal["integer"] = "integer"
-    field: Literal["textInput"] = "textInput"
-    
-    class Config:
-        title = "Edge Threshold"
-
-class OptionEdge(Config):
-    edgeThreshold: EdgeThreshold
-    name: Literal["Edge"] = "Edge"
-    value: Literal["Edge"] = "Edge"
-    type: Literal["string"] = "string"
-    field: Literal["option"] = "option"
-
-    class Config:
-        title = "Canny Edge"
+class PackageInputs(Inputs):
+    inputImage: InputImage
 
 
-#Single Filter Type Dropdown
-class ConfigFilterType(Config):
-    """
-    Select whether to Blur or detect Edges.
-    """
-    name: Literal["configFilterType"] = "configFilterType"
-    value: Union[OptionBlur, OptionEdge]
-    type: Literal["object"] = "object"
-    field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
-
-    class Config:
-        title = "Operation"
-        json_schema_extra = {
-            "shortDescription": "Filter Mode"
-        }
+class PackageConfigs(Configs):
+    degree: Degree
+    drawBBox: KeepSideBBox
 
 
-
-#Dual Filter Configs
-#Blend
-class BlendAlpha(Config):
-    """Mixing factor (0.0 - 1.0)"""
-    name: Literal["BlendAlpha"] = "BlendAlpha"
-    value: float = Field(default=0.5)
-    type: Literal["number"] = "number"
-    field: Literal["textInput"] = "textInput"
-
-    class Config:
-        title = "Alpha"
-
-class OptionBlend(Config):
-    blendAlpha: BlendAlpha
-    name: Literal["Blend"] = "Blend"
-    value: Literal["Blend"] = "Blend"
-    type: Literal["string"] = "string"
-    field: Literal["option"] = "option"
-
-    class Config:
-        title = "Blend Images"
+class PackageOutputs(Outputs):
+    outputImage: OutputImage
 
 
-#Concat
-class ConcatAxis(Config):
-    """0 for Vertical, 1 for Horizontal"""
-    name: Literal["ConcatAxis"] = "ConcatAxis"
-    value: int = Field(default=1)
-    type: Literal["integer"] = "integer"
-    field: Literal["textInput"] = "textInput"
-
-    class Config:
-        title = "Axis"
-
-class OptionConcat(Config):
-    concatAxis: ConcatAxis
-    name: Literal["Concat"] = "Concat"
-    value: Literal["Concat"] = "Concat"
-    type: Literal["string"] = "string"
-    field: Literal["option"] = "option"
-
-    class Config:
-        title = "Concatenate"
-
-
-#Dual Filter Mix Type Dropdown
-class ConfigMixType(Config):
-    """
-    Select how to combine the two images.
-    """
-    name: Literal["configMixType"] = "configMixType"
-    value: Union[OptionBlend, OptionConcat]
-    type: Literal["object"] = "object"
-    field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
-
-    class Config:
-        title = "Mix Mode"
-        json_schema_extra = {
-            "shortDescription": "Mixing Logic"
-        }
-
-
-
-#EXECUTORS (Single & Dual)
-#Executor 1: SingleFilter
-
-class SingleFilterExecutorInputs(Inputs):
-    inputImageOne: InputImageOne
-
-class SingleFilterExecutorConfigs(Configs):
-    configFilterType: ConfigFilterType
-
-class SingleFilterExecutorOutputs(Outputs):
-    outputImageOne: OutputImageOne
-
-class SingleFilterExecutorRequest(Request):
-    inputs: Optional[SingleFilterExecutorInputs]
-    configs: SingleFilterExecutorConfigs
+class PackageRequest(Request):
+    inputs: Optional[PackageInputs]
+    configs: PackageConfigs
 
     class Config:
         json_schema_extra = {
             "target": "configs"
         }
 
-class SingleFilterExecutorResponse(Response):
-    outputs: SingleFilterExecutorOutputs
 
-class SingleFilterExecutor(Config):
-    name: Literal["SingleFilterExecutor"] = "SingleFilterExecutor"
-    value: Union[SingleFilterExecutorRequest, SingleFilterExecutorResponse]
+class PackageResponse(Response):
+    outputs: PackageOutputs
+
+
+class PackageExecutor(Config):
+    name: Literal["Package"] = "Package"
+    value: Union[PackageRequest, PackageResponse]
     type: Literal["object"] = "object"
     field: Literal["option"] = "option"
 
     class Config:
-        title = "Single Filter Executor (1-in, 1-out)"
+        title = "Package"
         json_schema_extra = {
             "target": {
-                "value": 1  # Points to SingleFilter.py (2nd file)
+                "value": 0
             }
         }
 
-# Executor2:Dual Filter
-
-class DualFilterExecutorInputs(Inputs):
-    inputImageOne: InputImageOne
-    inputImageTwo: InputImageTwo
-
-class DualFilterExecutorConfigs(Configs):
-    configMixType: ConfigMixType
-
-class DualFilterExecutorOutputs(Outputs):
-    outputImageOne: OutputImageOne 
-    outputImageTwo: OutputImageTwo 
-
-class DualFilterExecutorRequest(Request):
-    inputs: Optional[DualFilterExecutorInputs]
-    configs: DualFilterExecutorConfigs
-
-    class Config:
-        json_schema_extra = {
-            "target": "configs"
-        }
-
-class DualFilterExecutorResponse(Response):
-    outputs: DualFilterExecutorOutputs
-
-class DualFilterExecutor(Config):
-    name: Literal["DualFilterExecutor"] = "DualFilterExecutor"
-    value: Union[DualFilterExecutorRequest, DualFilterExecutorResponse]
-    type: Literal["object"] = "object"
-    field: Literal["option"] = "option"
-
-    class Config:
-        title = "Dual Filter Executor (2-in, 2-out)"
-        json_schema_extra = {
-            "target": {
-                "value": 0 # Points to DualFilter.py (1st file)
-            }
-        }
-
-
-#Main Package Model
 
 class ConfigExecutor(Config):
-    """
-    Master selector for the Package.
-    """
     name: Literal["ConfigExecutor"] = "ConfigExecutor"
-    value: Union[SingleFilterExecutor, DualFilterExecutor]
+    value: Union[PackageExecutor]
     type: Literal["executor"] = "executor"
     field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
-    restart: Literal[True] = True
 
     class Config:
         title = "Task"
+        json_schema_extra = {
+            "target": "value"
+        }
 
 
 class PackageConfigs(Configs):
@@ -306,6 +145,6 @@ class PackageConfigs(Configs):
 
 
 class PackageModel(Package):
-    name: Literal["BlurredEdgedDemo"] = "BlurredEdgedDemo"
     configs: PackageConfigs
     type: Literal["component"] = "component"
+    name: Literal["Package"] = "Package"
