2eedd0020489a9f668fff97b76dd8d90ebd520ee
iamugurduman <127699986+iamugurduman@users.noreply.github.com>
2026-02-03 11:42:59 +0300

    feat:build_response hatası düzeltildi.

--- src/executors/BlurredEdged.py
+++ src/executors/BlurredEdged.py
@@ -11,8 +11,10 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from ..response import build_response
-from ..models.PackageModel import PackageModel
+from components.BlurredEdgedTest.src.utils.response import build_response
+from components.BlurredEdgedTest.src.models.PackageModel import PackageModel
+
+
 
 
 class BlurredEdged(Component):
